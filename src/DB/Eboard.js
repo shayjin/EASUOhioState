d47ac0344665ccfd561f409bd3eb1724b7617946
Wayne921030 <wayne921030@gmail.com>
2023-12-10 15:37:26 -0500

    testing

--- src/DB/Eboard.js
+++ src/DB/Eboard.js
@@ -1,25 +1,6 @@
 let Eboard = [
-    {
-        name: "Jen Lim",
-        position: "President",
-        major: "3rd-year Marketing",
-        picture: require("../sources/jenny.png"),
-        insta: "hyacinth._.ia"
-    },
-    {
-        name: "Derek Han",
-        position: "Student Advisor",
-        major: "3rd-year Industrial Engineering",
-        picture: require("../sources/derek.png"),
-        insta: "drek_han"
-    },
-    {
-      name: "Jay Shin",
-      position: "Vice President",
-      major: "4th-year Computer Science",
-      picture: require("../sources/jay.png"),
-      insta: "shayjin_"
-    },
+    
+    
     {
       name: "James Ahn",
       position: "Treasurer",
@@ -34,27 +15,8 @@ let Eboard = [
       picture: require("../sources/지윤.png"),
       insta: "jiyunl1"
     },
-    {
-      name: "Annabel Kim",
-      position: "Sponsor Relations",
-      major: "2nd-year Food Science",
-      picture: require("../sources/annabel.png"),
-      insta: "_annabelkim_"
-    },
-    {
-      name: "Ella Lee",
-      position: "Design Chair Lead",
-      major: "5th-year Information Systems",
-      picture: require("../sources/경민.png"),
-      insta: "kyungminnx"
-    },
-    {
-      name: "Minwoong Jeong",
-      position: "Event Chair Lead",
-      major: "4th-year Mechanical Engineering",
-      picture: require("../sources/웅민.png"),
-      insta: "minwoong.jeong"
-    },
+    
+    
     {
       name: "Wayne Chiang",
       position: "Technology Chair",
@@ -62,17 +24,19 @@ let Eboard = [
       picture: require("../sources/wayne.png"),
       insta: "waynechiang1030"
     },
-    {
-      name: "Kelly Yan",
-      position: "Design Chair",
-      major: "4th-year Computer Science",
-      picture: require("../sources/kelly.png"),
-      insta: "k.kyan"
-    },
+    
     {
       name: "James An",
       position: "Event Chair",
       major: "4th-year Biology",
+      picture: require("../sources/james2.png"),
+      insta: "ss_.jsn"
+    },
+
+    {
+      name: "Testing",
+      position: "Event Chair",
+      major: "3th-year Biology",
       picture: require("../sources/james2.png"),
       insta: "ss_.jsn"
     },

d3493536e57a8a29808f592309f0fd434c7b5248
Wayne921030 <wayne921030@gmail.com>
2024-01-08 10:17:18 -0500

    change logo

--- src/DB/Eboard.js
+++ src/DB/Eboard.js
@@ -1,28 +1,20 @@
 let Eboard = [
     
-    
-    {
-      name: "James Ahn",
-      position: "Treasurer",
-      major: "3rd-year Chemical Engineering",
-      picture: require("../sources/윤기.png"),
-      insta: "james.a_135"
-    },
     {
       name: "Jiyun Lee",
-      position: "Secretary / Outreach Chair",
+      position: "President",
       major: "2nd-year Computer Science",
       picture: require("../sources/지윤.png"),
       insta: "jiyunl1"
     },
     {
-      name: "Wayne Chiang",
-      position: "Technology Chair",
-      major: "2nd-year Computer Science",
-      picture: require("../sources/wayne.png"),
-      insta: "waynechiang1030"
+      name: "James Ahn",
+      position: "Vice President",
+      major: "3rd-year Chemical Engineering",
+      picture: require("../sources/윤기.png"),
+      insta: "james.a_135"
     },
-    
+
     {
       name: "James An",
       position: "Event Chair",
@@ -32,12 +24,69 @@ let Eboard = [
     },
 
     {
-      name: "Testing",
-      position: "Event Chair",
-      major: "3th-year Biology",
+      name: "Wayne Chiang",
+      position: "Technology Chair",
+      major: "2nd-year Computer Science",
+      picture: require("../sources/wayne.png"),
+      insta: "waynechiang1030"
+    },
+
+    {
+      name: "Joshua Baek",
+      position: "Sponsorship Chair",
+      major: "3rd-year Pharmacy",
       picture: require("../sources/james2.png"),
-      insta: "ss_.jsn"
+      insta: "josh._.baek"
+    },
+    
+    {
+      name: "Vienna Thai",
+      position: "Treasurer",
+      major: "2nd-year Finance",
+      picture: require("../sources/지윤.png"),
+      insta: "--"
+    },
+
+    {
+      name: "Vivian Qian",
+      position: "Event team",
+      major: "2nd-year Computer Science",
+      picture: require("../sources/wayne.png"),
+      insta: "vexists"
+    },
+    
+    {
+      name: "Yuting Che",
+      position: "Outreach team",
+      major: "2nd-year Computer Science",
+      picture: require("../sources/james2.png"),
+      insta: "yutingche0805"
+    },
+
+    {
+      name: "Kristine Zhong",
+      position: "Outreach team",
+      major: "2nd-year Business",
+      picture: require("../sources/james2.png"),
+      insta: "kristinez_11"
+    },
+
+    {
+      name: "Alicia Ping",
+      position: "Design team",
+      major: "2nd-year Computer Science",
+      picture: require("../sources/james2.png"),
+      insta: "almostalwaysalicia"
+    },
+    {
+      name: "Eunice Kim",
+      position: "Secretary",
+      major: "3rd-year Psycology / Minor in Korean",
+      picture: require("../sources/james2.png"),
+      insta: "eunicce.kim"
     }
+
+    
 ];
 
 export default Eboard;
